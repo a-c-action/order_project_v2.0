2a32e181174bc64c960acb642df31e2cb8b6f2e7
Tedu <1773298741@qq.con>
2019-06-25 20:49:04 +0800

    scoping

--- scooping/order/views.py
+++ scooping/order/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 
 def order(request):
     return  render(request,"order.html")
+
+def all_order_list(request):
+    # Order_from为订单模型类名
+    order_list=models.Order_from.objects.all()
+    return render(request,"order.html",locals())
+
+def payend_list(request):
+    # Pay_order为已付款订单模型类名
+    order_list=models.Pay_order.objects.filter()
+    return render(request,"order.html",locals())
+
+def nopay_list(request):
+    # Pay_order未付款订单模型类名
+    order_list=models.Order_completed.objects.filter()
+    return render(request,"order.html",locals())
