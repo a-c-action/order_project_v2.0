d78df9dea010df5d2546d349ee6241a12ec2e28a
Tedu <andor_song@foxmail.com>
2019-06-26 21:41:21 +0800

    修改

--- scooping/login/urls.py
+++ scooping/login/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.login)
+    url(r'^$',views.login),
+    url(r'^verify_code',views.verify_code_img),
 ]
--- scooping/register/urls.py
+++ scooping/register/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.register)
+    url(r'^$',views.register),
+    url(r'register_input$',views.register_input),
 ]
