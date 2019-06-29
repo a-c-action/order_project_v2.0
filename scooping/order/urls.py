f609db2af3536189d733af7eba23c65e2eb09bbc
Tedu <1773298741@qq.con>
2019-06-29 17:40:59 +0800

    order修改1

--- scooping/order/urls.py
+++ scooping/order/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r"^$",views.order),
+    url(r"^$",views.order1),
     url(r"^all",views.all_order_list),
     url(r"^payend",views.payend_list),
     url(r"^nopay",views.nopay_list),
--- scooping/order/views.py
+++ scooping/order/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from order import models
+from checkout import models
 
 # Create your views here.
 
-def order(request):
+def order1(request):
     return  render(request,"order.html")
 
 def all_order_list(request):
     # Order_from为订单模型类名
-    order_list=models.Order_list.objects.all()
+    order_list=models.Orderlist.objects.all()
     print(order_list)
     return render(request,"order.html",locals())
 
 def payend_list(request):
     # Pay_order为已付款订单模型类名
-    order = models.Order_table.objects.filter(check_type=0)
-    order_list = models.Order_list.objects.filter(check_id=order)
+    order = models.Ordertable.objects.filter(otype=0)
+    order_list = models.Orderlist.objects.filter(check_id=order)
     return render(request,"order.html",locals())
 
 def nopay_list(request):
     # Pay_order未付款订单模型类名
-    order=models.Order_table.objects.filter(check_type=1)
-    order_list = models.Order_list.objects.filter(check_id=order)
+    order=models.Ordertable.objects.filter(otype=1)
+    order_list = models.Orderlist.objects.filter(check_id=order)
     return render(request,"order.html",locals())
