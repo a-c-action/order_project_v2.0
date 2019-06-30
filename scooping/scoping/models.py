887ca19d70211bba4b0dcc3de9cee32c829fb9d8
Tedu <1773298741@qq.con>
2019-06-30 13:24:41 +0800

    order修改1

--- scooping/scoping/models.py
+++ scooping/scoping/models.py
@@ -10,7 +10,6 @@ from userinfo.models import UserProfile
 class Order_from(models.Model):
     check_data=models.DateField("订餐日期")
     check_time=models.TimeField("订单时段")
-    check_id=models.CharField("订单编号",max_length=50)
     check_person=models.CharField("人数",max_length=50)
     check_phone=models.CharField("电话号码",max_length=50)
     check_table=models.CharField("桌号",max_length=50)
--- scooping/scoping/views.py
+++ scooping/scoping/views.py
@@ -2,18 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import response
 from django.contrib import messages
+from . import models
 
 
-# Create your views here.
+# Create your views here
 
 
 def book_table(request):
+    if request.method=="GET":
+        return render(request,"index.html")
     if request.method == "POST":
+        value=request.COOKIES.get('user')
         time = request.POST.get("time")
         date = request.POST.get("date")
         person = request.POST.get("person")
         table = request.POST.get("table")
         phone = request.POST.get("phone")
+        uid=models.UserProfile.objects.get(uname=value)
+        order=models.Order_from.objects.create(check_data=date,check_time=time,check_person=person,
+                                                check_phone=phone, check_table=table, uid=uid)
+        order.save()
         html="time:"+time+"date:"+date+"person:"+person+"table:"+table+"phone:"+phone
 
 
