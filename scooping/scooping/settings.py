a9467e7fbae9a52077fa04d039c9917a95f4d672
Tedu <andor_song@foxmail.com>
2019-07-02 08:29:30 +0800

    comify

--- scooping/scooping/settings.py
+++ scooping/scooping/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mymiddleware.checklogin.MyMiddleWare',
 ]
 
 ROOT_URLCONF = 'scooping.urls'
