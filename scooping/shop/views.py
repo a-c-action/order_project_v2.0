fbbd5c80182d4a058b10f18fe2f1c7e7df47aa09
Tedu <lvze@tedu.cn>
2019-07-02 10:32:29 +0800

    shop

--- scooping/shop/views.py
+++ scooping/shop/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from single.models import Menu
 
+from . import models
 # Create your views here.
-def shop(request):
-    return render(request,"shop.html")
+# def shop(request):
+#     return render(request,"shop.html")
+
+
+from django.core.paginator import Paginator
+def food_page(request):
+    '''分页显示当前的菜单数据'''
+    foods =Menu.objects.all()
+    paginator = Paginator(foods, 9)
+    print(paginator.page_range)
+
+    cur_page = request.GET.get('page', 1)
+    page = paginator.page(cur_page)
+    return render(request, 'shop.html', locals())
+
+def category(request):
+    data=request.GET["ctype"]
+    foodf=Menu.objects.filter(ctype=data).all()
+    return render(request,'shop.html',locals())
