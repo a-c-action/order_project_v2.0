4e80d957c5d3207b00eb2e8bdff43f91b18d313c
Tedu <1773298741@qq.con>
2019-07-03 21:16:18 +0800

    ordertable

--- scooping/scoping/views.py
+++ scooping/scoping/views.py
@@ -7,26 +7,31 @@ from django.core import serializers
 import json
 
 
+
 # Create your views here
 
 
 def book_table(request):
-    if request.method=="GET":
-        return render(request,"index.html")
-    elif request.method == "POST":
-        value=request.session['user']["uaccount"]
-        time = request.POST.get("time")
-        date = request.POST.get("date")
-        person = request.POST.get("person")
-        table = request.POST.get("table")
-        phone = request.POST.get("phone")
-        uid=models.UserProfile.objects.get(uname=value)
-        order=models.Order_from.objects.create(check_data=date,check_time=time,check_person=person,
-                                                check_phone=phone, check_table=table, uid=uid)
-        # order.save()
-        # html="time:"+time+"date:"+date+"person:"+person+"table:"+table+"phone:"+phone
-        # print(html)
-        return render(request,"index.html")
+    value=request.session['user']["uaccount"]
+    time = request.POST.get("timetable")
+    date = request.POST.get("datatable")
+    person = request.POST.get("personnum")
+    table = request.POST.get("tablenum")
+    phone = request.POST.get("phone")
+    uid=models.UserProfile.objects.get(uname=value)
+    order=models.Order_from.objects.create(check_data=date,check_time=time,check_person=person,
+                                            check_phone=phone, check_table=table, uid=uid)
+    # order.save()
+    html="time:"+time+"date:"+date+"person:"+person+"table:"+table+"phone:"+phone
+    print(html)
+    tableall = ['桌号1', '桌号2', '桌号3', '桌号5', '桌号6', '桌号8', '桌号9', '桌号10']
+    tables = models.Order_from.objects.filter(check_data=date, check_time=time)
+    print(tables)
+    for table in tables:
+        tableall.remove(table.check_table)
+    str = json.dumps(tableall)
+    # return render(request,"index.html")
+    return HttpResponse(str)
 
 
 def checktable(request):
--- scooping/static/js/ordertable.js
+++ scooping/static/js/ordertable.js
@@ -10,7 +10,7 @@ $(function(){
             "timetable":timetable,
             "datatable":datatable
         }
-        $.post("/scoping/checktable",msg,
+        $.get("/scoping/checktable",msg,
         function(data){
                console.log(data,typeof(data));
                data=JSON.parse(data);
@@ -34,7 +34,7 @@ $(function(){
             "timetable":timetable,
             "datatable":datatable
         }
-        $.post("/scoping/checktable",msg,
+        $.get("/scoping/checktable",msg,
         function(data){
                console.log(data,typeof(data));
                data=JSON.parse(data);
@@ -46,6 +46,36 @@ $(function(){
                $("#tablenum").html(html)
                console.log(html);
         })
+
+    })
+    //提交
+    $("#tablesubmit").click(function(){
+        var timetable=$('#timepickerit').val();
+        var datatable=$('#datepicker1').val();
+        var personnum=$('#personnum').val();
+        var tablenum=$('#tablenum').val();
+        var phone=$('#phone').val();
+//        var url="/scoping/checktable?time='+time+'&data='+data;
+        console.log(timetable,datatable)
+        var msg={
+            "timetable":timetable,
+            "datatable":datatable,
+            "personnum":personnum,
+            "tablenum":tablenum,
+            "phone":phone,
+            "csrfmiddlewaretoken":$("[name='csrfmiddlewaretoken']").val()
+        }
+        $.post("/scoping/booktable",msg,
+        function(data){
+                alert("预定成功")
+                data=JSON.parse(data);
+                console.log(data,typeof(data));
+                var html="";
+                for(i=0;i<data.length;i++){
+                    html+="<option>"+data[i]+"</option>"
+                }
+                $("#tablenum").html(html)
+                })
 
     })
 })
