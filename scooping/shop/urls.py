6921a72c2a38d095f44cc1c742ae935809661533
Tedu <1773298741@qq.con>
2019-07-05 19:03:48 +0800

    shop

--- scooping/shop/urls.py
+++ scooping/shop/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r"^$",views.food_page,name="food"),
-    url(r"^query/",views.query),
-
+    url(r"^food_server/",views.server01),
+    url(r"^search_server/",views.server02),
 ]
--- scooping/shop/views.py
+++ scooping/shop/views.py
@@ -12,20 +12,39 @@ from . import models
 from django.core.paginator import Paginator
 def food_page(request):
     '''分页显示当前的菜单数据'''
-    foods =Menu.objects.all()
+    foods =Menu.objects.filter(ctype="简餐").all()
     paginator = Paginator(foods, 9)
     print(paginator.page_range)
 
     cur_page = request.GET.get('page', 1)
     page = paginator.page(cur_page)
+    # print(locals())
     return render(request, 'shop.html', locals())
 
-def query(request):
-    name = request.GET['name']
-    foods=Menu.objects.filter(cname=name).all()
-    msg=""
-    for food in foods:
-        msg += "%s_%s_%s_%s_%s_%s|" % (food.id, food.ctype,
-                                       food.cname,food.cprice,food.cmarket_price,food.pic)
-    msg = msg[0:-1]
-    return HttpResponse(msg)
+def server01(request):
+    '''分页显示当前的菜单数据'''
+    ctype=request.GET['ctype']
+    # print(ctype)
+
+    foods =Menu.objects.filter(ctype=ctype).all()
+    print(foods)
+    paginator = Paginator(foods, 9)
+    print(paginator.page_range)
+
+    cur_page = request.GET.get('page', 1)
+    page = paginator.page(cur_page)
+    jsonStr=serializers.serialize("json",page)
+    return HttpResponse(jsonStr)
+
+def server02(request):
+    content=request.GET["content"]
+    foods=Menu.objects.filter(cname__contains=content).all()
+    print(foods)
+    paginator = Paginator(foods, 9)
+    print(paginator.page_range)
+
+    cur_page = request.GET.get('page', 1)
+    page = paginator.page(cur_page)
+    jsonStr = serializers.serialize("json", page)
+    return HttpResponse(jsonStr)
+
