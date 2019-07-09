213e2da4bac4fae2c6c11a9c6f52ff5c1b615a92
Tedu <1773298741@qq.con>
2019-07-09 08:41:29 +0800

    chackout

--- scooping/checkout/views.py
+++ scooping/checkout/views.py
@@ -5,7 +5,7 @@ import json
 from . import models
 from single.models import Menu
 from userinfo.models import UserProfile
-
+from django.db.models import Count
 from decimal import *
 
 from django.http import HttpResponse
@@ -16,6 +16,13 @@ def homepage(request):
     users = UserProfile.objects.filter(uname=username)
     print(users)
     carts=models.Shoppingcart.objects.filter(sid=users)
+    princes={}
+
+    for cart in carts:
+        number=int(cart.number)
+        price=cart.cprice*number
+        princes[cart.cname]=price
+        print(price)
     return render(request, 'checkout.html',locals())
 
 
