9a905f321d95fc6034a5136821f73fde2e28327b
Tedu <1773298741@qq.con>
2019-07-10 18:52:28 +0800

    shop

--- scooping/shop/views.py
+++ scooping/shop/views.py
@@ -13,40 +13,19 @@ from . import models
 
 from django.core.paginator import Paginator
 def food_page(request):
-    '''分页显示当前的菜单数据'''
     foods =Menu.objects.filter(ctype="简餐").all()
-    # paginator = Paginator(foods, 9)
-    # print(paginator.page_range)
-    #
-    # cur_page = request.GET.get('page', 1)
-    # page = paginator.page(cur_page)
-    # print(locals())
     return render(request, 'shop.html', locals())
 
 def server01(request):
-    '''分页显示当前的菜单数据'''
-    ctype=request.GET['ctype']
-    # print(ctype)
 
+    ctype=request.GET['ctype']
     foods =Menu.objects.filter(ctype=ctype).all()
-    # print(foods)
-    # paginator = Paginator(foods, 9)
-    # print(paginator.page_range)
-    #
-    # cur_page = request.GET.get('page', 1)
-    # page = paginator.page(cur_page)
     jsonStr=serializers.serialize("json",foods)
     return HttpResponse(jsonStr)
 
 def server02(request):
     content=request.GET["content"]
     foods=Menu.objects.filter(cname__contains=content).all()
-    # print(foods)
-    # paginator = Paginator(foods, 9)
-    # print(paginator.page_range)
-    #
-    # cur_page = request.GET.get('page', 1)
-    # page = paginator.page(cur_page)
     jsonStr = serializers.serialize("json", foods)
     return HttpResponse(jsonStr)
 
