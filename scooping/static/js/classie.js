6f337da72129daca6277c789be866f844c6d2a3a
Tedu <1773298741@qq.con>
2019-07-12 17:47:29 +0800

    gdshgh

--- scooping/static/js/classie.js
+++ scooping/static/js/classie.js
@@ -28,7 +28,6 @@ var hasClass, addClass, removeClass;
 if ( 'classList' in document.documentElement ) {
 
   hasClass = function( elem, c ) {
-  console.log(elem)
     return elem.classList.contains( c );
   };
   addClass = function( elem, c ) {
